6536528241a8f632f3e316de46a0b26897db7b65
deepak404found <deepakyadu404@gmail.com>
2022-06-16 15:50:31 +0530

    updated

--- codes/day-1.py
+++ codes/day-1.py
@@ -1,4 +1,15 @@
-student= ['om',19]
-if (student[0]=='om'):
-    if (student [1]>= 19):
-        print (student[0], "is a good boy")
+studnet1={"name":'deepak', "age":20}
+studnet2={"name":'lokesh', "age":20}
+studnet3={"name":'nomesh', "age":20}
+name='name1'
+
+print("Student1 name is", studnet1['name'], "and age is", studnet1['age'])
+
+print("Student2 name is", studnet2['name'], "and age is", studnet2['age'])
+
+print("Student3 name is", studnet3['name'], "and age is", studnet3['age'])
+
+# print("Student1 name is", studnet1['name'], "and age is", studnet1['age'])
+#
+# print(f"Studnet name is {studnet2['name']}, and age is {studnet2['age']}")
+n=int(input("enter your number: "))
---
+++ codes/day-4.py
@@ -0,0 +1,8 @@
+studnet1={"name":'deepak', "age":20}
+studnet2={"name":'lokesh', "age":20}
+studnet3={"name":'nomesh', "age":20}
+
+print("Student name is", studnet1['name'], "and age is", studnet1['age'])
+
+
+# print(f"Studnet name is f{studnet1['name']}, and age is f{studnet1['age']}")
