ccdb487c76d95300e79cef1e88c5ca0e6a60b27c
deepak404found <deepakyadu404@gmail.com>
2022-06-19 13:41:16 +0530

    day-7 code added

--- codes/day-7.py
+++ codes/day-7.py
@@ -1,1 +1,8 @@
 # list comprehension.
+num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+even = (num for num  in range(0,11) if num % 2 == 0)
+odd = (num for num in range(0,11) if num % 2 != 0)
+
+print ("Eeve numbers: ",  list(even))
+print ("odd numbers: ", list(odd))
