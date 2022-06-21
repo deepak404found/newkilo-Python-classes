16e9ebc868f93214da60af28736d962f8d860c4b
deepak404found <deepakyadu404@gmail.com>
2022-06-21 06:27:16 +0530

    some updates

--- codes/day-7.py
+++ codes/day-7.py
@@ -1,8 +1,7 @@
 # list comprehension.
 num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-even = (num for num  in range(0,11) if num % 2 == 0)
-odd = (num for num in range(0,11) if num % 2 != 0)
-
-print ("Eeve numbers: ",  list(even))
-print ("odd numbers: ", list(odd))
+even = [num for num  in range(0,11) if num % 2 == 0]
+odd = [num for num in range(0,11) if num % 2 != 0]
+print ("Eeve numbers: ",  even)
+print ("odd numbers: ", odd)
--- codes/guessTheNumber1.py
+++ codes/guessTheNumber1.py
@@ -1,22 +1,42 @@
 import random
 
-num = random.randint(0, 10)
+users = []
+play = 'yes'
 
-attempt = 4
-while attempt > 0:
-   
-    user_input = int(input('Enter Number: '))
-    if user_input == num:
-        print('You Won!')
-        break
-    elif user_input > num:
-        print(f'{user_input} is greater.\nRemaining attempts: {attempt}.')
-        attempt -= 1
+while(play=='yes'):
+    username = input("enter the username: ")
+    num = random.randint(0, 10)
+    
+    attempt = 3
+    while attempt >= 0:
+    
+        user_input = int(input('Enter Number: '))
+        print(attempt)
+        if user_input == num:
+            print('You Won!')
+            break
+        elif attempt == 0:
+            print(f'\nRemaining attempts: {attempt}.')
+            print('You Lost!')
+            break
+        elif user_input > num:
+            print(f'{user_input} is greater.\nRemaining attempts: {attempt}.')
+            attempt -= 1
 
-    elif user_input < num:
-        print(f'{user_input} is smaller.\nRemaining attempts: {attempt}.')
-        attempt -= 1
-
-    else:
-        print('Something went wrong!')
-        break
+        elif user_input < num:
+            print(f'{user_input} is smaller.\nRemaining attempts: {attempt}.')
+            attempt -= 1
+        else:
+            print('Something went wrong!')
+            break
+            
+    users.append({'username':username,'score':attempt+1})
+    play = input('Do you want to play again? (yes/no): ')
+    
+# show scoreboard
+print(users)
+print('\nScoreboard:')
+print('Username\tScore')
+print('-'*10)
+print('\n'.join(f'{user["username"]}\t\t{user["score"]}' for user in users))
+print('\n')
